0436237155eadfbcf64da70771280bed0ac48bb8
Angelos Charalambidis <acharal@iit.demokritos.gr>
2014-07-15 15:24:52 +0300

    Stat

---
+++ src/main/java/gr/demokritos/iit/irss/semagrow/rdf/RDFSTHolesHistogram.java
@@ -0,0 +1,10 @@
+package gr.demokritos.iit.irss.semagrow.rdf;
+
+import gr.demokritos.iit.irss.semagrow.stholes.STHolesHistogram;
+
+/**
+ * Created by angel on 7/15/14.
+ */
+public class RDFSTHolesHistogram extends STHolesHistogram {
+
+}
--- src/main/java/gr/demokritos/iit/irss/semagrow/rdf/Stat.java
+++ src/main/java/gr/demokritos/iit/irss/semagrow/rdf/Stat.java
@@ -1,7 +1,14 @@
 package gr.demokritos.iit.irss.semagrow.rdf;
 
+import java.util.List;
+
 /**
  * Created by angel on 7/15/14.
  */
 public class Stat {
+
+    public Long frequency;
+
+    public List<Long> distinctCount;
+
 }
