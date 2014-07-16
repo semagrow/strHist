c97a380a8338b643864c2bbeb61c846262294a33
Efi <ippatia2@gmail.com>
2014-07-16 16:05:41 +0300

    CalendarRange and initial RDFLiteralRange intersection

--- src/main/java/gr/demokritos/iit/irss/semagrow/rdf/RDFLiteralRange.java
+++ src/main/java/gr/demokritos/iit/irss/semagrow/rdf/RDFLiteralRange.java
@@ -1,8 +1,9 @@
 package gr.demokritos.iit.irss.semagrow.rdf;
 
-import gr.demokritos.iit.irss.semagrow.api.Range;
-import gr.demokritos.iit.irss.semagrow.api.Rangeable;
+import gr.demokritos.iit.irss.semagrow.api.*;
+import org.openrdf.model.URI;
 import org.openrdf.model.Value;
+import org.openrdf.model.vocabulary.XMLSchema;
 
 /**
  * Created by angel on 7/15/14.
@@ -11,12 +12,48 @@ public class RDFLiteralRange
         implements Range<Value>, Rangeable<RDFLiteralRange>
 {
 
-    public boolean isUnit() {
-        return false;
+    private URI valueType;
+    private Range<?> range;
+
+    public RDFLiteralRange(URI valueType, Range<?> range) {
+        this.valueType = valueType;
+        this.range = range;
     }
 
-    public RDFLiteralRange intersection(RDFLiteralRange rect) {
-        return null;
+    public boolean isUnit() {
+        return range.isUnit();
+    }
+
+    public RDFLiteralRange intersection(RDFLiteralRange literalRange) {
+
+
+        RDFLiteralRange res = null;
+        if (valueType.equals(literalRange.getValueType()))
+        {
+
+            if (valueType.equals(XMLSchema.INTEGER)) {
+                res = new RDFLiteralRange(valueType,
+                        ((IntervalRange<Integer>) range).intersection(
+                                (IntervalRange<Integer>) literalRange.getRange()));
+            } else if (valueType.equals(XMLSchema.LONG)) {
+                res = new RDFLiteralRange(valueType,
+                        ((IntervalRange<Integer>) range).intersection(
+                                (IntervalRange<Integer>) literalRange.getRange()));
+
+            } else if (valueType.equals(XMLSchema.STRING)) {
+                res = new RDFLiteralRange(valueType,
+                        ((PrefixRange) range).intersection(
+                                (PrefixRange) literalRange.getRange()));
+            } else if (valueType.equals(XMLSchema.DATETIME)) {
+                res = new RDFLiteralRange(valueType,
+                        ((CalendarRange) range).intersection(
+                                (CalendarRange) literalRange.getRange()));
+            }
+        }
+
+
+
+        return res;
     }
 
     public boolean contains(RDFLiteralRange rect) {
@@ -26,5 +63,21 @@ public class RDFLiteralRange
     @Override
     public boolean intersects(RDFLiteralRange rect) {
         return false;
+    }
+
+    public URI getValueType() {
+        return valueType;
+    }
+
+    public void setValueType(URI valueType) {
+        this.valueType = valueType;
+    }
+
+    public Range<?> getRange() {
+        return range;
+    }
+
+    public void setRange(Range<?> range) {
+        this.range = range;
     }
 }
