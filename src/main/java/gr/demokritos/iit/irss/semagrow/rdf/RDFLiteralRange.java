b3604e6cf275500a54e4628b3ce514e7fe7f77f3
Angelos Charalambidis <acharal@iit.demokritos.gr>
2014-07-16 12:53:37 +0300

    importing openrdf-sesame

--- src/main/java/gr/demokritos/iit/irss/semagrow/rdf/RDFLiteralRange.java
+++ src/main/java/gr/demokritos/iit/irss/semagrow/rdf/RDFLiteralRange.java
@@ -2,12 +2,13 @@ package gr.demokritos.iit.irss.semagrow.rdf;
 
 import gr.demokritos.iit.irss.semagrow.api.Range;
 import gr.demokritos.iit.irss.semagrow.api.Rangeable;
+import org.openrdf.model.Value;
 
 /**
  * Created by angel on 7/15/14.
  */
 public class RDFLiteralRange
-        implements Range<Object>, Rangeable<RDFLiteralRange>
+        implements Range<Value>, Rangeable<RDFLiteralRange>
 {
 
     public boolean isUnit() {
