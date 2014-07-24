e9f228812641df4397b18479badc63c64dcfd0a7
unknown <nickolaszoulis@gmail.com>
2014-07-24 17:24:51 +0300

    removed unnecessary stuff

--- src/main/java/gr/demokritos/iit/irss/semagrow/parsing/HistogramIO.java
+++ src/main/java/gr/demokritos/iit/irss/semagrow/parsing/HistogramIO.java
@@ -23,10 +23,8 @@ public class HistogramIO<R extends Rectangle<R>> {
 
 	private String path;
 	private STHolesBucket<R> rootBucket;
+    private STHolesHistogram histogram;
 
-    public STHolesHistogram getHistogram() {
-        return histogram;
-    }
 
    	public HistogramIO(String path, STHolesHistogram histogram) {
 		setPath(path);
@@ -35,10 +33,8 @@ public class HistogramIO<R extends Rectangle<R>> {
 
 
 	public void write() {
-		writeJSOn();
-//		writeNonBinary();
-//		writeBinary();
-	}
+        writeJSOn();
+    }
 
 
     public static STHolesBucket read(String path) {
@@ -47,7 +43,7 @@ public class HistogramIO<R extends Rectangle<R>> {
 
 
 	/**
-	 * Writes the histogram into a file in JSOn format.
+	 * Writes the histogram into a file in jSON format.
 	 */
 	private void writeJSOn() {
 		FileWriter fw;
@@ -235,126 +231,26 @@ public class HistogramIO<R extends Rectangle<R>> {
     }// getStatistics
 
 
-	/**
-	 * Writes the histogram into a file in a non human-readable form.
-	 */
-	public void writeBinary() {
-		ObjectOutputStream oos = null;		
-		
-		try {			
-			oos = new ObjectOutputStream(new FileOutputStream(getPath() + ".ser"));
-			
-			oos.writeObject(getRootBucket());			
-			
-			oos.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}// writeBinary
-
-
-	/**
-	 * Writes the histogram into a file in a human-readable form.
-	 */
-	private void writeNonBinary() {
-		BufferedWriter bw = null;
-		
-		try {
-			bw = new BufferedWriter(new FileWriter(getPath() + ".txt"));
-
-			// Write root bucket.			
-			bw.write(rootBucket.toString());
-			
-			bw.write("\n{\n");
-			// Get root's children.
-			Collection<STHolesBucket<R>> children = rootBucket.getChildren();
-
-			System.err.println("Children: " + children.size());
-
-			// Foreach child, call write method recursively.
-			for (STHolesBucket<R> child : children) {
-				System.err.println("Children of child>> " + child.getChildren().size());
-				write(child, bw);
-			}
-			
-			bw.write("\n}\n");
-			bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}// writeNonBinary	
-
-
-	/**
-	 * Recursively writes a bucket and its children if exist.
-	 */
-	private void write(STHolesBucket<R> bucket, BufferedWriter bw) {
-		
-		try { 
-			bw.write("\n{\n");
-			bw.write(bucket.toString());
-			
-			for (STHolesBucket<R> child : bucket.getChildren()) 			
-				write(child, bw);		
-			
-			bw.write("\n}\n");
-		} catch (IOException e) {e.printStackTrace();}	
-
-	}// write
-	
-	
-	/**
-	 * Reads the binary file and instantiates the rootBucket of the Histogram.	
-	 */
-	public STHolesBucket<R> readBinary(String path) {
-		STHolesBucket<R> rootBucket = null;
-		
-		ObjectInputStream ois = null;
-		
-		try {
-			ois = new ObjectInputStream(new FileInputStream(path));
-						
-			rootBucket = (STHolesBucket<R>)ois.readObject();			
-			
-			ois.close();
-		} catch (FileNotFoundException e) {			
-			e.printStackTrace();
-		} catch (IOException e) {			
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-		
-		return rootBucket;
-	}// readBinary
-	
-
 	/*
 	 * Getters & Setters.
 	 */
 	public String getPath() {
 		return path;
 	}
-
-
 	public void setPath(String path) {
 		this.path = path;
 	}
-
-
 	public STHolesBucket<R> getRootBucket() {
 		return rootBucket;
 	}
-
-
 	public void setRootBucket(STHolesBucket<R> rootBucket) {
 		this.rootBucket = rootBucket;
 	}
-
+    public STHolesHistogram getHistogram() {
+        return histogram;
+    }
     public void setHistogram(STHolesHistogram histogram) {
         this.histogram = histogram;
     }
-
-    private STHolesHistogram histogram;
 
 }
