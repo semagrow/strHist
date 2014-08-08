9ae8d5f940a458bfc8bd39985130a65fdbca3968
Nickolas Zoulis <nickolaszoulis@gmail.com>
2014-08-09 02:34:10 +0300

    optimizing generators

---
+++ evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/CreateNativeStore.java
@@ -0,0 +1,50 @@
+package gr.demokritos.iit.irss.semagrow;
+
+import info.aduna.iteration.Iterations;
+import org.openrdf.model.Statement;
+import org.openrdf.model.URI;
+import org.openrdf.model.impl.ValueFactoryImpl;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.openrdf.repository.RepositoryResult;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.rio.RDFFormat;
+import org.openrdf.rio.Rio;
+import org.openrdf.sail.nativerdf.NativeStore;
+
+import java.io.File;
+
+/**
+ * Created by Nick on 08-Aug-14.
+ */
+public class CreateNativeStore {
+
+    public static void main(String[] args) throws RepositoryException {
+
+//        String dataFolder = args[0];
+//        String nativeStoreFolder = args[1];
+
+        String dataFolder = "C:\\Users\\Nick\\Downloads\\filtered\\";
+        String nativeStoreFolder = "src\\main\\resources\\native_store\\";
+
+        // Create a local Sesame Native Store.
+        Repository nativeRep = new SailRepository(new NativeStore(new File(nativeStoreFolder)));
+        nativeRep.initialize();
+
+        File[] files = new File(dataFolder).listFiles();
+
+        RepositoryConnection conn = nativeRep.getConnection();
+
+        for (File file : files) {
+            System.out.println(file.getName());
+
+            try {
+                conn.add(file, file.getName(), RDFFormat.forFileName(file.getName()));
+            } catch(Exception e) {
+                System.out.println(e.toString());
+                System.out.println("skipping " + file.getName());
+            }
+        }
+    }
+}
--- evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/TestMainQueryFeedbackGenerate.java
+++ evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/TestMainQueryFeedbackGenerate.java
@@ -1,6 +1,7 @@
 package gr.demokritos.iit.irss.semagrow;
 
 import gr.demokritos.iit.irss.semagrow.api.qfr.QueryRecord;
+import org.openrdf.repository.RepositoryException;
 
 import java.io.IOException;
 
@@ -9,14 +10,16 @@ import java.io.IOException;
  */
 public class TestMainQueryFeedbackGenerate {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, RepositoryException {
 
 
         String uniqueSubjectData = "C:\\Users\\Nick\\Downloads\\sorted\\sorted";
         String filteredDataFolder = "C:\\Users\\Nick\\Downloads\\filtered\\";
         String outputDataFolder = "C:\\Users\\Nick\\git\\sthist\\src\\main\\resources\\data\\";
+        String nativeStoreFolder = "src\\main\\resources\\native_store\\";
 
-        QueryFeedbackGenerator qfg = new QueryFeedbackGenerator(uniqueSubjectData, filteredDataFolder, outputDataFolder);
+        QueryFeedbackGenerator qfg = new QueryFeedbackGenerator(uniqueSubjectData, filteredDataFolder,
+                outputDataFolder, nativeStoreFolder);
 
         Iterable<QueryRecord> trainingSet = qfg.generateTrainingSet(1);
         for (QueryRecord qr : trainingSet)
