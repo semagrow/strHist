98bd2dd33a17a9107a86611741925cb4a63df992
Stasinos Konstantopoulos <stasinos@users.sourceforge.net>
2014-08-13 00:17:15 +0300

    genTrain also converts to num ranges batch-by-batch

--- scripts/genTrain.sh
+++ scripts/genTrain.sh
@@ -4,7 +4,10 @@ i=1;
 
 while [[ 1==1 ]]; do
     F=$(printf "%05d\n" $i)
-    echo "java -cp ./lib/*:./lib/cp gr.demokritos.iit.irss.semagrow.TestMainQueryFeedbackGenerate 50 sorted f/ train/b$F/"
+
+    java -cp ./lib/*:./lib/cp gr.demokritos.iit.irss.semagrow.TestMainQueryFeedbackGenerate 50 sorted f/ "train/b$F/"
+    java -cp ./lib/*:./lib/cp gr.demokritos.iit.irss.semagrow.RDFtoNumQueryConverter -s sorted -t "train/b$F" -o "train_num/b$F"
+
     i=$((i+1))
 done
 
