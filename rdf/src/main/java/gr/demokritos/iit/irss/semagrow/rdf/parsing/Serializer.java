8bec00fd82bc71c4a43f23238e540a176efb8826
nickozoulis <nickolas.zoulis@gmail.com>
2014-10-01 17:20:17 +0300

    eleon necessities

--- rdf/src/main/java/gr/demokritos/iit/irss/semagrow/rdf/parsing/Serializer.java
+++ rdf/src/main/java/gr/demokritos/iit/irss/semagrow/rdf/parsing/Serializer.java
@@ -23,12 +23,21 @@ import java.io.FileOutputStream;
 public class Serializer {
 
     static class VOID {
-        static String NAMESPACE = "http://rdfs.org/ns/void#";
+        static final String NAMESPACE = "http://rdfs.org/ns/void#";
     }
 
     private int level;
     private Model model;
-    private String histogramNamespace = "http://www.semagrow.eu/metadata/histogram/";
+    private String histogramNamespace = "http://www.semagrow.eu/metadata/histogram/",
+                   // Eleon centered variables
+                   eleonRootNamespace = "http://rdf.iit.demokritos.gr/2013/sevod#datasetTop",
+                   eleonFacet = "http://rdf.iit.demokritos.gr/2013/sevod#facet",
+                   eleonFacetProperty = "http://rdf.iit.demokritos.gr/2013/sevod#propertyFacet",
+                   eleonIssuedBy = "http://www.w3.org/2007/05/powder-s#issuedby",
+                   eleonUser = "http://eleon.iit.demokritos.gr/user#nick";
+
+
+
     private String outputPath;
     private RDFFormat format;
 
@@ -46,7 +55,7 @@ public class Serializer {
     public Serializer(String format, String outputPath) {
 
         level = 0;
-        this.outputPath = outputPath + "histVoID.ttl";
+        this.outputPath = outputPath;
         this.format = Rio.getWriterFormatForMIMEType(format);
         model = new TreeModel();
     }
@@ -54,16 +63,28 @@ public class Serializer {
 
     public void serialize(STHolesHistogram<RDFRectangle> histogram) {
 
+        // Eleon load fix: Add a root node with name 'datasetTop'
+        Resource bucketResource = createResource(eleonRootNamespace);
+        model.add(bucketResource, RDF.TYPE, dataset);
+        model.add(bucketResource, title, createLiteral(bucketResource.toString()));
+        model.add(bucketResource, subset, createResource(1));
+        model.add(bucketResource, createURI("http://purl.org/dc/terms/creator"), createLiteral("nick"));
+        eleon(bucketResource);
+
         serializeBucket(histogram.getRoot(), createResource(++level), 0, true);
-        write(outputPath);
+        writeToFile(outputPath);
     }
 
+    private void eleon(Resource bucketResource) {
+        model.add(bucketResource, createURI(eleonFacet), createURI(eleonFacetProperty));
+        model.add(bucketResource, createURI(eleonIssuedBy), createURI(eleonUser));
+    }
 
-    private void write(String outputPath) {
+    private void writeToFile(String outputPath) {
 
         FileOutputStream out = null;
         try {
-            out = new FileOutputStream(outputPath);
+            out = new FileOutputStream(outputPath + "histVoID.ttl");
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
@@ -87,20 +108,29 @@ public class Serializer {
             bucketResource = createResource(bucketResource.toString(), num);
 
         model.add(bucketResource, RDF.TYPE, dataset);
-        model.add(bucketResource, title, createLiteral(bucketResource.toString()));
         model.add(bucketResource, triples, createLiteral(bucket.getStatistics().getFrequency()));
         model.add(bucketResource, distinctSubjects, createLiteral(bucket.getStatistics().getDistinctCount().get(0)));
         model.add(bucketResource, properties, createLiteral(bucket.getStatistics().getDistinctCount().get(1)));
         model.add(bucketResource, distinctObjects, createLiteral(bucket.getStatistics().getDistinctCount().get(2)));
+        eleon(bucketResource);
 
+        String titleStr = "";
         // Subject's Range
-        for (String s : bucket.getBox().getSubjectRange().getPrefixList())
+        for (String s : bucket.getBox().getSubjectRange().getPrefixList()) {
             model.add(bucketResource, uriRegexPattern, createLiteral(s));
+            titleStr += "/" + getLastPrefix(s) + " ";
+        }
         // Predicate's Ranges
-        for (String s : bucket.getBox().getPredicateRange().getItems())
+        for (String s : bucket.getBox().getPredicateRange().getItems()) {
             model.add(bucketResource, property, createURI(s));
+            titleStr += "/" + getLastPrefix(s) + " ";
+        }
 
         //TODO: Object's Ranges
+        titleStr += "?o";
+
+        // Title is the subject(prefix)-predicate-object
+        model.add(bucketResource, title, createLiteral(titleStr));
 
         // Declare each child as subset and serialize recursively each bucket.
         int count = 0;
@@ -109,6 +139,12 @@ public class Serializer {
             model.add(bucketResource, subset, createResource(bucketResource.toString(), count));
             serializeBucket(b, bucketResource, count, false);
         }
+    }
+
+    private String getLastPrefix(String s) {
+
+        String[] splits = s.split("/");
+        return splits[splits.length - 1];
     }
 
 
@@ -121,6 +157,9 @@ public class Serializer {
         return ValueFactoryImpl.getInstance().createURI(nameSpaceAndLevel + "_" + num);
     }
 
+    private Resource createResource(String nameSpace) {
+        return ValueFactoryImpl.getInstance().createURI(nameSpace);
+    }
 
     private URI createURI(String nameSpace, String localName) {
         return ValueFactoryImpl.getInstance().createURI(nameSpace, localName);
--- rdf/src/main/java/gr/demokritos/iit/irss/semagrow/rdf/parsing/TestMainSerializer.java
+++ rdf/src/main/java/gr/demokritos/iit/irss/semagrow/rdf/parsing/TestMainSerializer.java
@@ -12,7 +12,7 @@ public class TestMainSerializer {
     public static void main(String[] args) {
 
         // Read histogram from file.
-        STHolesBucket<RDFRectangle> rootBucket = HistogramIO.readJSON("/home/nickozoulis/git/sthist/rdf/src/main/resources/histogram.json.txt");
+        STHolesBucket<RDFRectangle> rootBucket = HistogramIO.readJSON("/home/nickozoulis/git/sthist/rdf/src/main/resources/big_histogram.json.txt");
         System.out.println(rootBucket);
 
         STHolesHistogram<RDFRectangle> histogram = new STHolesHistogram();
