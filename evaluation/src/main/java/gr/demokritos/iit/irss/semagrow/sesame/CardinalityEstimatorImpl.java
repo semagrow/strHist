350cfc866a2571f5dc6c62e0d0614f3dca05d873
Angelos Charalambidis <acharal@iit.demokritos.gr>
2014-10-12 19:43:11 +0300

    impl of cost and card estimators

--- evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/CardinalityEstimatorImpl.java
+++ evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/CardinalityEstimatorImpl.java
@@ -4,8 +4,7 @@ import gr.demokritos.iit.irss.semagrow.api.Histogram;
 import gr.demokritos.iit.irss.semagrow.rdf.RDFRectangle;
 import org.openrdf.model.Value;
 import org.openrdf.query.BindingSet;
-import org.openrdf.query.algebra.StatementPattern;
-import org.openrdf.query.algebra.TupleExpr;
+import org.openrdf.query.algebra.*;
 
 /**
  * Created by angel on 10/11/14.
@@ -18,11 +17,56 @@ public class CardinalityEstimatorImpl implements CardinalityEstimator {
         this.histogram = histogram;
     }
 
-    public long getCardinality(TupleExpr tupleExpr, BindingSet bindings) {
-        if (tupleExpr instanceof StatementPattern)
-            return getCardinality((StatementPattern) tupleExpr, bindings);
+    public long getCardinality(TupleExpr expr, BindingSet bindings) {
+
+        if (expr instanceof StatementPattern)
+            return getCardinality((StatementPattern)expr, bindings);
+        else if (expr instanceof Union)
+            return getCardinality((Union)expr, bindings);
+        else if (expr instanceof Filter)
+            return getCardinality((Filter)expr, bindings);
+        else if (expr instanceof Projection)
+            return getCardinality((Projection)expr, bindings);
+        else if (expr instanceof Slice)
+            return getCardinality((Slice)expr, bindings);
+        else if (expr instanceof Join)
+            return getCardinality((Join)expr, bindings);
+        else if (expr instanceof LeftJoin)
+            return getCardinality((LeftJoin)expr, bindings);
         else
             return 0;
+    }
+
+    public long getCardinality(Slice slice, BindingSet bindings) {
+        long card = getCardinality(slice.getArg(), bindings);
+        return Math.min(card, slice.getLimit());
+    }
+
+    public long getCardinality(Join join, BindingSet bindings) {
+        long card1 = getCardinality(join.getLeftArg(), bindings);
+        long card2 = getCardinality(join.getRightArg(), bindings);
+        double sel = 0.5;
+        return (long)(card1 * card2 * sel);
+    }
+
+    public long getCardinality(LeftJoin join, BindingSet bindings) {
+        long card1 = getCardinality(join.getLeftArg(), bindings);
+        long card2 = getCardinality(join.getRightArg(), bindings);
+        double sel = 0.5;
+        return Math.max(card1, (long)(card1 * card2 * sel));
+    }
+
+    public long getCardinality(Filter filter, BindingSet bindings) {
+        long card = getCardinality(filter.getArg(), bindings);
+        return (long)(card * 0.5);
+    }
+
+    public long getCardinality(UnaryTupleOperator op, BindingSet bindings) {
+        return getCardinality(op, bindings);
+    }
+
+    public long getCardinality(Union union, BindingSet bindings) {
+        return getCardinality(union.getLeftArg(), bindings) + getCardinality(union.getRightArg(), bindings);
     }
 
     public long getCardinality(StatementPattern pattern, BindingSet bindings) {
@@ -30,6 +74,7 @@ public class CardinalityEstimatorImpl implements CardinalityEstimator {
         return histogram.estimate(toRectangle(pattern, bindings));
     }
 
+    //TODO
     private RDFRectangle toRectangle(StatementPattern pattern, BindingSet bindings) {
         Value sVal = pattern.getPredicateVar().getValue();
         Value pVal = pattern.getPredicateVar().getValue();
--- evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/CostEstimatorImpl.java
+++ evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/CostEstimatorImpl.java
@@ -1,6 +1,7 @@
 package gr.demokritos.iit.irss.semagrow.sesame;
 
-import org.openrdf.query.algebra.TupleExpr;
+import org.openrdf.query.algebra.*;
+import org.openrdf.query.impl.EmptyBindingSet;
 
 /**
  * Created by angel on 10/11/14.
@@ -18,5 +19,26 @@ public class CostEstimatorImpl implements CostEstimator {
         return 0;
     }
 
+    public double getCost(StatementPattern pattern) {
+        return cardinalityEstimator.getCardinality(pattern, EmptyBindingSet.getInstance());
+    }
 
+    public double getCost(Join join) {
+        long leftCard = cardinalityEstimator.getCardinality(join.getLeftArg(), EmptyBindingSet.getInstance());
+        return getCost(join.getLeftArg()) + leftCard*getCost(join.getRightArg());
+    }
+
+
+    public double getCost(Union union) {
+        return cardinalityEstimator.getCardinality(union, EmptyBindingSet.getInstance()) +
+               getCost(union.getLeftArg()) + getCost(union.getRightArg());
+    }
+
+    public double getCost(UnaryTupleOperator op) {
+        return getCost(op.getArg());
+    }
+
+    public double getCost(BinaryTupleOperator op) {
+        return getCost(op.getLeftArg()) + getCost(op.getRightArg());
+    }
 }
