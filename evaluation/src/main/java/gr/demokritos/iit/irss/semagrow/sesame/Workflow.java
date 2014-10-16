e2ef6d27e4bec64c6e7e0304b00aa63e510b3776
nickozoulis <nickolas.zoulis@gmail.com>
2014-10-16 20:25:10 +0300

    cleansing workflow

--- evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/Workflow.java
+++ evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/Workflow.java
@@ -2,7 +2,12 @@ package gr.demokritos.iit.irss.semagrow.sesame;
 
 import com.bigdata.rdf.sail.BigdataSail;
 import com.bigdata.rdf.sail.BigdataSailRepository;
-import org.openrdf.query.*;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import org.openrdf.query.MalformedQueryException;
+import org.openrdf.query.QueryEvaluationException;
+import org.openrdf.query.QueryLanguage;
+import org.openrdf.query.TupleQuery;
 import org.openrdf.repository.Repository;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
@@ -18,56 +23,68 @@ import java.util.Properties;
  */
 public class Workflow {
 
-//    static final String TSHistogramPath = "/mnt/ocfs2/IDF_data/journals/exp_triples/histogram_data/data_";
-//
-//    static String q = "prefix dc: <http://purl.org/dc/elements/1.1/> " +
-//            "prefix semagrow: <http://www.semagrow.eu/rdf/> " +
-//            "select * { " +
-//            "?pub dc:title ?title ." +
-//            "?pub semagrow:year \"1975\" ." +
-//            "?pub semagrow:origin \"US\" . }";
+    private static String q = "prefix dc: <http://purl.org/dc/elements/1.1/> " +
+            "prefix semagrow: <http://www.semagrow.eu/rdf/> " +
+            "select * { " +
+            "?pub dc:title ?title ." +
+            "?pub semagrow:year \"%s\" ." +
+            "?pub semagrow:origin \"%s\" . }";
+    // Use it like this : String.format(q, "2012", "US");
 
-    static String q = "select * {<http://agris.fao.org/aos/records/XF7590017> ?p ?o}";
-    static final String TSHistogramPath = "/home/nickozoulis/data_";
+    private static int startDate, endDate;
+    private static String tripleStorePath = "/mnt/ocfs2/IDF_data/journals/exp_triples/histogram_data/data_";
+    public static String logOutputPath;
 
+    /**
+     * s = Starting date, e = Ending Date, l = LogOutput path
+     * @param args
+     * @throws RepositoryException
+     * @throws IOException
+     */
+    static public void main(String[] args) throws RepositoryException, IOException, NumberFormatException {
+        OptionParser parser = new OptionParser("s:e:l:");
+        OptionSet options = parser.parse(args);
 
-    static public void main(String[] args) throws RepositoryException, IOException {
+        if (options.hasArgument("s") && options.hasArgument("e") && options.hasArgument("l")) {
+            startDate = Integer.parseInt(options.valueOf("s").toString());
+            endDate = Integer.parseInt(options.valueOf("e").toString());
+            if (startDate > endDate) System.exit(1);
+            logOutputPath = options.valueOf("l").toString();
+        }
+        else System.exit(1);
 
-        // For all dates from 1975 to 2013
-        for (int i=1975; i<=2013; i++) {
+        runExperiment();
+    }
+
+    private static void runExperiment() throws RepositoryException, IOException {
+        for (int i=startDate; i<=endDate; i++) {
+            // -- Query Evaluation
             try {
-//                RepositoryConnection conn = getFedRepository(getRepository(i)).getConnection();
-                RepositoryConnection conn = getRepository(i).getConnection();
+                RepositoryConnection conn = getFedRepository(getRepository(i)).getConnection();
 
                 TupleQuery query = conn.prepareTupleQuery(QueryLanguage.SPARQL, q);
 
-                TupleQueryResult res = query.evaluate();
-
-//                while (res.hasNext()) {
-//                    BindingSet bs = res.next();
-//                    System.out.println("-- "+bs.toString());
-//                }
-
-
-
+                query.evaluate();
             } catch (MalformedQueryException mqe) {mqe.printStackTrace();
             } catch (QueryEvaluationException e) {e.printStackTrace();}
 
-            // the evaluation of the query will write logs (query feedback).
-            //TODO: Parse the logs and fill an Iterable<RDFQueryRecord> ??
+            // -- Histogram Training
 
-//            STHolesHistogram<RDFRectangle> histogram = new STHolesHistogram();
-//            histogram.refine(Iterable);
+//            // The evaluation of the query will write logs (query feedback).
+//            List<RDFQueryRecord> listQueryRecords = new LogParser(logOutputPath).parse();
+//
+//            STHolesHistogram<RDFRectangle> histogram = new STHolesHistogram<RDFRectangle>();
+//            histogram.refine(listQueryRecords);
 
-//            maybe write histogram to file in void or json format
+//            Maybe write histogram to file in void or json format
 
-//            compare with actual cardinalities using ActualCardinalityEstimator
+            // -- Histogram Testing
 
-            break;//TODO: Remove after testing.
+//            Compare with actual cardinalities using ActualCardinalityEstimator
+
+
         }
-
     }
-
 
     private static Repository getFedRepository(Repository actual) throws RepositoryException {
         Sail sail = new TestSail(actual);
@@ -76,11 +93,10 @@ public class Workflow {
         return repo;
     }
 
-
     private static Repository getRepository(int year) throws RepositoryException, IOException {
         Properties properties = new Properties();
 
-        File journal = new File(TSHistogramPath + year + ".jnl");
+        File journal = new File(tripleStorePath + year + ".jnl");
 
         properties.setProperty(
                 BigdataSail.Options.FILE,
