300343401e24536edef5fa4bf29067785b288eae
nickozoulis <nickolas.zoulis@gmail.com>
2014-10-19 11:43:34 +0300

    debug actual cardinality estimator

--- evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/ActualCardinalityEstimator.java
+++ evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/ActualCardinalityEstimator.java
@@ -1,7 +1,10 @@
 package gr.demokritos.iit.irss.semagrow.sesame;
 
 import org.openrdf.query.*;
+import org.openrdf.query.algebra.Projection;
 import org.openrdf.query.algebra.TupleExpr;
+import org.openrdf.query.parser.ParsedTupleQuery;
+import org.openrdf.queryrender.sparql.SPARQLQueryRenderer;
 import org.openrdf.repository.RepositoryConnection;
 import org.openrdf.repository.RepositoryException;
 
@@ -18,45 +21,32 @@ public class ActualCardinalityEstimator implements CardinalityEstimator {
 
     @Override
     public long getCardinality(TupleExpr expr, BindingSet bindings) {
+        long c = 0;
 
-        String[] exprNames = (String[])expr.getBindingNames().toArray();
+        if (expr instanceof Projection)
+            expr = ((Projection)expr).getArg();
 
-        String s, p, o = "";
-
-        if (bindings.hasBinding(exprNames[0]))
-            s = (bindings.getBinding(exprNames[0])).getValue().stringValue();
-        else
-            s = "?" + exprNames[0];
-
-        if (bindings.hasBinding(exprNames[1]))
-            p = (bindings.getBinding(exprNames[1])).getValue().stringValue();
-        else
-            p = "?" + exprNames[1];
-
-        if (bindings.hasBinding(exprNames[2]))
-            o = (bindings.getBinding(exprNames[2])).getValue().stringValue();
-        else
-            o = "?" + exprNames[2];
-
-        String q = "select count(*) as card { " + s + " " + p + " " + o + " .}";
-        long card = 0;
-
-        TupleQuery query = null;
-        TupleQueryResult res = null;
+        ParsedTupleQuery parsedQuery = new ParsedTupleQuery(expr);
+        SPARQLQueryRenderer renderer = new SPARQLQueryRenderer();
 
         try {
-            query = cnx.prepareTupleQuery(QueryLanguage.SPARQL, q);
-            res = query.evaluate();
+
+            String queryString = "SELECT (count(*) AS c) WHERE " + renderer.render(parsedQuery);
+
+
+            TupleQuery query = cnx.prepareTupleQuery(QueryLanguage.SPARQL, queryString);
+            TupleQueryResult res = query.evaluate();
 
             if (res.hasNext())
-                card = Long.parseLong(res.next().getBinding("card").getValue().stringValue());
+                c = Long.parseLong(res.next().getBinding("card").getValue().stringValue());
 
         } catch (NumberFormatException e) {e.printStackTrace();
         } catch (RepositoryException e) {e.printStackTrace();
         } catch (MalformedQueryException e) {e.printStackTrace();
-        } catch (QueryEvaluationException e) {e.printStackTrace();}
+        } catch (QueryEvaluationException e) {e.printStackTrace();
+        } catch (Exception e) {e.printStackTrace();}
 
-        return card;
+        return c;
     }
 
 }
