68a4e6b2200d8e6be3cc97c62eb851765ebd01b1
Angelos Charalambidis <acharal@iit.demokritos.gr>
2014-10-26 11:59:08 +0200

    interval tests

--- api/src/main/java/gr/demokritos/iit/irss/semagrow/base/range/IntervalRange.java
+++ api/src/main/java/gr/demokritos/iit/irss/semagrow/base/range/IntervalRange.java
@@ -167,80 +167,11 @@ public class IntervalRange implements RangeLength<Integer>, Rangeable<IntervalRa
         return "[" + low + "-" + high + "]";
     }
 
-
-    public static void main(String [] args){
-        IntervalRange myRange = new IntervalRange(1,6);
-        int item1 = 3;
-        int item2 = 7;
-        boolean res1 = myRange.includes(item1);
-        boolean res2 = myRange.includes(item2);
-
-        // Test getters
-        System.out.println("My range is (" + myRange.getLow() + "," +
-                myRange.getHigh() + ")");
-        //Test includes item method
-        if (res1)
-            System.out.println("My range includes " + item1);
-        else
-            System.out.println("Test failed");
-
-        if (!res2)
-            System.out.println("My range does not contain " + item2);
-        else
-            System.out.println("Test failed");
-        //Test includes range method
-        IntervalRange testRange1 = new IntervalRange(3,5);
-        IntervalRange testRange2 = new IntervalRange(1,8);
-
-        res1 = myRange.contains(testRange1);
-        res2 = myRange.contains(testRange2);
-
-        System.out.println("Range " + myRange.toString() + "includes range "
-                + testRange1.toString() + ":" + res1);
-        System.out.println("Range " + myRange.toString() + "includes range "
-                + testRange2.toString() + ":" + res2);
-
-        //Test intersection method
-        testRange1 = new IntervalRange(2,7);
-        testRange2 = new IntervalRange(0,8);
-        IntervalRange testRange3 = new IntervalRange(8,10);
-
-
-        IntervalRange intersection1 = myRange.intersection(
-                testRange1);
-        IntervalRange intersection2 = myRange.intersection(
-                testRange2);
-        IntervalRange intersection3 = myRange.intersection(
-                testRange3);
-
-        System.out.println("Intersection of range " + myRange.toString() +
-                "and range " + testRange1.toString() + ": " + intersection1);
-        System.out.println("Intersection of range " + myRange.toString() +
-                "and range " + testRange2.toString() + ":" + intersection2);
-        System.out.println("Intersection of range " + myRange.toString() +
-                "and range " + testRange3.toString() + ":" + intersection3);
-
-        //Test minus
-        IntervalRange b = new IntervalRange(5,10);
-        IntervalRange p1 = new IntervalRange(4,6);
-        IntervalRange p2 = new IntervalRange(8,11);
-        IntervalRange p3 = new IntervalRange(7,9);
-        IntervalRange p4 = new IntervalRange(4,11);
-
-
-        System.out.println(b + " minus " + p1 + " = " + b.minus(p1));
-        System.out.println(b + " minus " + p2 + " = " + b.minus(p2));
-        System.out.println(b + " minus " + p3 + " = " + b.minus(p3));
-        System.out.println(b + " minus " + p4 + " = " + b.minus(p4));
-
-        //Test tightRange
-        IntervalRange r1 = new IntervalRange(14,20);
-
-        System.out.println("Tight range of " + b + " and " +
-                p2 + " is " + b.tightRange(p2));
-        System.out.println("Tight range of " + b + " and " +
-                r1 + " is " + b.tightRange(r1));
-
-
+    public boolean equals(Object o) {
+        if (o instanceof IntervalRange) {
+            IntervalRange i = (IntervalRange)o;
+            return low == i.low && high == i.high;
+        }
+        return false;
     }
 }
--- api/src/test/java/gr/demokritos/iit/irss/semagrow/base/range/PrefixRangeTest.java
+++ api/src/test/java/gr/demokritos/iit/irss/semagrow/base/range/PrefixRangeTest.java
@@ -2,34 +2,151 @@ package gr.demokritos.iit.irss.semagrow.base.range;
 
 import junit.framework.TestCase;
 
+import java.util.ArrayList;
+
 public class PrefixRangeTest extends TestCase {
 
-    public void testIncludes() throws Exception {
+    public void testIncludesIncludedSubstring() throws Exception {
 
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        myRangePrefixList.add("http://a/");
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
+        String item1 = "http://a/b/c/d";
+        assertTrue(myRange.includes(item1));
+    }
+
+    public void testIncludesIncludedIdentity() throws Exception {
+
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        String item1 = "http://a/";
+        myRangePrefixList.add(item1);
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
+        assertTrue(myRange.includes(item1));
+    }
+
+    public void testIncludesNotIncluded() throws Exception {
+
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        myRangePrefixList.add("http://a/");
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
+        String item2 = "http://b/c";
+        assertFalse(myRange.includes(item2));
     }
 
     public void testContains() throws Exception {
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        String item1 = "http://a/";
+        myRangePrefixList.add(item1);
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
 
+        ArrayList<String> testRange1PrefixList = new ArrayList<String>();
+        testRange1PrefixList.add("http://a/b");
+        PrefixRange testRange1 = new PrefixRange(testRange1PrefixList);
+
+        assertTrue(myRange.contains(testRange1));
     }
 
-    public void testIntersects() throws Exception {
+    public void testContainsNotContained() throws Exception {
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        String item1 = "http://a/";
+        myRangePrefixList.add(item1);
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
 
+        ArrayList<String> testRange2PrefixList = new ArrayList<String>();
+        testRange2PrefixList.add("http://b");
+
+        PrefixRange testRange2 = new PrefixRange(testRange2PrefixList);
+
+        assertFalse(myRange.contains(testRange2));
+    }
+
+    public void testIntersection() throws Exception {
+
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        String item1 = "http://a/";
+        myRangePrefixList.add(item1);
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
+
+        ArrayList<String> testRange1PrefixList = new ArrayList<String>();
+        testRange1PrefixList.add("http://a/b");
+        PrefixRange testRange1 = new PrefixRange(testRange1PrefixList);
+
+        PrefixRange result = myRange.intersection(testRange1);
+        assertTrue(myRange.intersects(testRange1));
+        assertTrue(result.equals(testRange1));
+    }
+
+    public void testIntersection2() throws Exception {
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        String item1 = "http://a/";
+        myRangePrefixList.add(item1);
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
+
+        ArrayList<String> testRange2PrefixList = new ArrayList<String>();
+        testRange2PrefixList.add("http://");
+
+        PrefixRange testRange2 = new PrefixRange(testRange2PrefixList);
+
+        PrefixRange result = myRange.intersection(testRange2);
+        assertTrue(myRange.intersects(testRange2));
+        assertTrue(result.equals(myRange));
+    }
+
+    public void testIntersection3() throws Exception {
+
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        String item1 = "http://a/";
+        myRangePrefixList.add(item1);
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
+
+        ArrayList<String> testRange3PrefixList = new ArrayList<String>();
+        testRange3PrefixList.add("http://b");
+        PrefixRange testRange3 = new PrefixRange(testRange3PrefixList);
+
+        assertFalse(myRange.intersects(testRange3));
+
+        PrefixRange result = myRange.intersection(testRange3);
+        assertTrue(result.isEmpty());
+        assertFalse(result.isInfinite());
     }
 
     public void testTightRange() throws Exception {
 
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        myRangePrefixList.add("http://a/");
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
+
+        ArrayList<String> testRange3PrefixList = new ArrayList<String>();
+        testRange3PrefixList.add("http://b");
+        testRange3PrefixList.add("http://c");
+        PrefixRange testRange3 = new PrefixRange(testRange3PrefixList);
+
+        PrefixRange result = myRange.tightRange(testRange3);
+
+        assertTrue(result.contains(myRange));
+        assertTrue(result.contains(testRange3));
     }
 
     public void testExpand() throws Exception {
 
     }
 
-    public void testIntersection() throws Exception {
-
-    }
-
     public void testMinus() throws Exception {
+        ArrayList<String> myRangePrefixList = new ArrayList<String>();
+        myRangePrefixList.add("http://a/");
+        PrefixRange myRange = new PrefixRange(myRangePrefixList);
 
+        ArrayList<String> testRange3PrefixList = new ArrayList<String>();
+        testRange3PrefixList.add("http://b");
+        testRange3PrefixList.add("http://c");
+        PrefixRange testRange3 = new PrefixRange(testRange3PrefixList);
+
+        PrefixRange result = myRange.tightRange(testRange3);
+
+        assertTrue(result.contains(myRange));
+        assertTrue(result.contains(testRange3));
+
+        //result.minus(testRange1);
     }
 
     public void testIsUnit() throws Exception {
