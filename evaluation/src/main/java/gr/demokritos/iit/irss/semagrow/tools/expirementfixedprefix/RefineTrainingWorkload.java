3ad00bdfb7ec8f8b45c70a23d66e19882106dc08
nickozoulis <nickolas.zoulis@gmail.com>
2014-11-20 16:30:59 +0200

    Decoupling Refine and Evaluate procedures

--- evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/tools/expirementfixedprefix/RefineTrainingWorkload.java
+++ evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/tools/expirementfixedprefix/RefineTrainingWorkload.java
@@ -1,7 +1,100 @@
 package gr.demokritos.iit.irss.semagrow.tools.expirementfixedprefix;
 
+import gr.demokritos.iit.irss.semagrow.api.QueryLogRecord;
+import gr.demokritos.iit.irss.semagrow.api.qfr.QueryRecord;
+import gr.demokritos.iit.irss.semagrow.rdf.RDFSTHolesHistogram;
+import gr.demokritos.iit.irss.semagrow.stholes.STHolesHistogram;
+import gr.demokritos.iit.irss.semagrow.tools.Utils;
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.RepositoryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.OpenOption;
+import java.nio.file.StandardOpenOption;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * Created by nickozoulis on 20/11/2014.
  */
 public class RefineTrainingWorkload {
+
+    static final Logger logger = LoggerFactory.getLogger(RefineTrainingWorkload.class);
+    static final OpenOption[] options = {StandardOpenOption.CREATE, StandardOpenOption.APPEND};
+     private static ExecutorService executors;
+
+    private static String inputPath, outputPath;
+    private static int year;
+
+    public static void main(String[] args) throws IOException, RepositoryException {
+//        executeExperiment();
+        OptionParser parser = new OptionParser("y:i:o:");
+        OptionSet options = parser.parse(args);
+
+        if (options.hasArgument("y") && options.hasArgument("i") && options.hasArgument("o")) {
+            year = Integer.parseInt(options.valueOf("y").toString());
+            inputPath = options.valueOf("i").toString();
+            outputPath = options.valueOf("o").toString();
+
+            executeExperiment();
+        } else {
+            logger.error("Invalid arguments");
+            System.exit(1);
+        }
+    }
+
+    private static void executeExperiment() throws IOException, RepositoryException {
+        executors = Executors.newCachedThreadPool();
+
+        refineAndEvaluate(Utils.getRepository(year, inputPath));
+
+        executors.shutdown();
+    }
+
+    private static void refineAndEvaluate(Repository repo) throws IOException, RepositoryException {
+        // Load Feedback
+        Collection<QueryLogRecord> logs = Utils.parseFeedbackLog("/var/tmp/" + year + "/" + year + "_log.ser");
+        Collection<QueryRecord> queryRecords = Utils.adaptLogs(logs, year, executors);
+
+        logger.info("Starting refining histogram: " + year);
+        RepositoryConnection conn;
+
+        try {
+            conn = repo.getConnection();
+
+            // Refine histogram according to the feedback
+            RDFSTHolesHistogram histogram = refineHistogram(queryRecords.iterator(), year, 0);
+
+            conn.close();
+        } catch (RepositoryException e) {
+            e.printStackTrace();
+        }
+
+        repo.shutDown();
+    }
+
+    private static RDFSTHolesHistogram refineHistogram(Iterator<QueryRecord> listQueryRecords, int year, int iteration) {
+        RDFSTHolesHistogram histogram;
+
+        if (iteration == 0)
+            histogram = Utils.loadPreviousHistogram(outputPath, year);
+        else
+            histogram = Utils.loadCurrentHistogram(outputPath, year);
+
+        logger.info("Refining histogram " + year);
+        ((STHolesHistogram)histogram).refine(listQueryRecords);
+        logger.info("Refinement is over.");
+
+        Utils.serializeHistogram(histogram, outputPath, year);
+
+        return histogram;
+    }
+
 }
