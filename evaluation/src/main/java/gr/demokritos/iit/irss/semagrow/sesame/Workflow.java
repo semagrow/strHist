a98c6c127fccd7d1e3ffc35e29e1a55d33643d8e
nickozoulis <nickolas.zoulis@gmail.com>
2014-12-08 17:36:31 +0200

    some static variables, no use

--- evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/Workflow.java
+++ evaluation/src/main/java/gr/demokritos/iit/irss/semagrow/sesame/Workflow.java
@@ -1,10 +1,16 @@
 
 package gr.demokritos.iit.irss.semagrow.sesame;
 
+import java.util.List;
+
 /**
  * Created by angel on 10/11/14.
  */
 public class Workflow {
+
+    private static List<String> agroTerms;
+    public static String HISTPATH;
+    private static String TSPATH;
 
     static public void main(String[] args) {
 
